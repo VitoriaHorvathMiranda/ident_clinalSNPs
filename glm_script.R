681537bd3f02e5b32050a32469c24c014bfd0a36
Vitoria_Horvath_Miranda <vitoria.h.miranda@gmail.com>
2023-01-10 15:54:02 -0300

    filtra para fora os snps com profundidade de leitura = 0; atualiza o código com data.table

--- glm_script.R
+++ glm_script.R
@@ -6,31 +6,50 @@ library(data.table)
 parser <- ArgumentParser(description= "computes a glm for each snp")
 parser$add_argument('--timePops', '-tPops',
                     help = 'outputs from join_time_pops_script.R')
-parser$add_argument('output', '-o',
+parser$add_argument('--output', '-o',
                     help = 'table with glm, coefficient and p-value')
 xargs<- parser$parse_args()
 
-joined_pops <- fread(file = xargs$timePops)
+joined_pops <- xargs$timePops
+
+#reads data and joins chrom and position
+joined_pops <- fread(file = joined_pops)
 joined_pops[, position2 := paste(chrom, position, sep = ":")]
 joined_pops <- joined_pops[, !c("chrom", "position")]
 
-GLM_TABLE <- joined_pops %>%
-  group_by(position2) %>% 
-  nest() %>% 
-  mutate(n_pops = map_dbl(data, nrow)) %>%
-  filter(n_pops > 2) %>%
-  select(!n_pops) %>%
-  mutate(models = map(data, ~ glm(freq~latitude, weights = NE,
-                                  data = .x,
-                                  family = binomial()))) %>%
-  mutate(coefficients = map_dbl(models, ~coef(.x) %>% pluck("latitude"))) %>%
-  mutate(p_value = map_dbl(models, 
-                           ~summary(.x) %>% 
-                             pluck("coefficients") %>% pluck(8))) 
+#data.table function for nesting:
+group_nest_dt <- function(dt, ..., .key = "data"){
+  stopifnot(is.data.table(dt))
+  by <- substitute(list(...))
+  dt <- dt[, list(list(.SD)), by = eval(by)]
+  setnames(dt, old = "V1", new = .key)
+  dt
+}
+
+#filters calls with depth = 0
+#nest data by snp
+#filter out snps that were only called in two or less pops
+nested_snps <- joined_pops[depth>0,][,group_nest_dt(.SD, position2)][, 
+                                       n_pops := purrr::map_dbl(data, nrow)][
+                                       n_pops>2,][,
+                                       !c("n_pops"), with = FALSE]
+#runs glm for each snp
+nested_snps[, models := purrr::map(data, ~ glm(freq~latitude, 
+                                               weights = NE,
+                                               data = .x,
+                                               family = binomial()))]
+
+#gets lat coefficient
+nested_snps[, coefficients := purrr::map_dbl(models, ~coef(.x) %>% pluck("latitude")) ]
+
+#gets p-value
+nested_snps[, p_value := purrr::map_dbl(models, 
+                                         ~summary(.x) %>% 
+                                           pluck("coefficients") %>% pluck(8))]
 
 glm_table <- xargs$output
 
-saveRDS(GLM_TABLE, file = glm_table)
+saveRDS(nested_snps, file = glm_table)
 
 
 
--- model_test.R
+++ model_test.R
@@ -2,32 +2,58 @@ library(data.table)
 library(tidyverse)
 library(broom)
 
-
+#11994070 - 2L
 pop_test <- fread("/dados/time_clines/output/SNPs/joined97.tsv")
 
-pop_test[, position2 := paste(chrom, position, sep = ":")]
+pop_test_lite <- pop_test[chrom == "2L",]
 
-pop_test <- pop_test[order(position2),] 
+pop_test_lite[, position2 := paste(chrom, position, sep = ":")]
 
-pop_test <- pop_test[, !c("chrom", "position")] 
-
-pop_test_lite <- pop_test[0:500,]
+pop_test_lite <- pop_test_lite[, !c("chrom", "position")] 
 
 
-#tidyverse try:
+
+#tidyverse and data.table try:
+#data.table function for nesting:
+group_nest_dt <- function(dt, ..., .key = "data"){
+  stopifnot(is.data.table(dt))
+  by <- substitute(list(...))
+  dt <- dt[, list(list(.SD)), by = eval(by)]
+  setnames(dt, old = "V1", new = .key)
+  dt
+}
+
+nested_test <- pop_test_lite[depth>0,][, group_nest_dt(.SD, position2)]
+
+#test[10000:10010,][position2 == "2L:622893", ] %>% pluck(2)
+
+nested_test_filtered <- nested_test[,n_pops := purrr::map_dbl(data, nrow)][
+  n_pops>2,][,!c("n_pops"), with = FALSE]
+
+
+nested_test_filtered[, models := purrr::map(data, ~ glm(freq~latitude, 
+                                    weights = NE,
+                                    data = .x,
+                                    family = binomial()))]
+
+nested_test_filtered[, coefficients := purrr::map_dbl(models, ~coef(.x) %>% pluck("latitude")) ]
+nested_test_filtered[, p_value := purrr::map_dbl(models, 
+                                         ~summary(.x) %>% 
+                                           pluck("coefficients") %>% pluck(8))]
+
 #fited_models <- 
-magica_table <- pop_test_lite %>%
+magica_table <- pop_test_lite[depth>0,] %>%
   group_by(position2) %>% 
   nest() %>% 
+  mutate(n_pops = map_dbl(data, nrow)) %>%
+  filter(n_pops > 2) %>%
+  select(!n_pops) %>%
   mutate(models = map(data, ~ glm(freq~latitude, weights = NE,
                 data = .x,
                 family = binomial()))) 
 
 
 magica_table %>%
-  mutate(n_pops = map_dbl(data, nrow)) %>%
-  filter(n_pops > 2) %>%
-  select(!n_pops) %>%
   mutate(coefficients = map_dbl(models, ~coef(.x) %>% pluck("latitude"))) %>%
   mutate(p_value = map_dbl(models, 
                        ~summary(.x) %>% 
@@ -43,7 +69,19 @@ magica_table %>%
 
 
 
-
+GLM_TABLE <- pop_test %>%
+  group_by(position2) %>% 
+  nest() %>% 
+  mutate(n_pops = map_dbl(data, nrow)) %>%
+  filter(n_pops > 2) %>%
+  select(!n_pops) %>%
+  mutate(models = map(data, ~ glm(freq~latitude, weights = NE,
+                                  data = .x,
+                                  family = binomial()))) %>%
+  mutate(coefficients = map_dbl(models, ~coef(.x) %>% pluck("latitude"))) %>%
+  mutate(p_value = map_dbl(models, 
+                           ~summary(.x) %>% 
+                             pluck("coefficients") %>% pluck(8))) 
 
 
 
